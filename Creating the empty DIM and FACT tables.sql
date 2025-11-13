998c879e5b93cd0e494ee9a29a34bbe4aca5dc12
Görkem <118932678+GorkemEfe1@users.noreply.github.com>
2025-11-13 12:52:45 -0500

    Delete staging tables from SQL script
    Removed staging tables for departments, students, courses, professors, semesters, and enrollments.

--- Creating the empty DIM and FACT tables.sql
+++ Creating the empty DIM and FACT tables.sql
@@ -9,8 +9,8 @@ CREATE TABLE Dim_Semester (
 	Semester_Key INT PRIMARY KEY IDENTITY(1,1),
     Semester_ID_Source VARCHAR(50) NOT NULL,
     Semester_Year VARCHAR(9) NOT NULL,
-    Semester_Name VARCHAR(20) NOT NULL
- );
+    Semester_Name VARCHAR(20)Â NOTÂ NULL
+Â );
 CREATE TABLE Dim_Professor (
  	Professor_Key INT PRIMARY KEY IDENTITY(1,1),
     Professor_ID_Source VARCHAR(50) NOT NULL,
@@ -53,55 +53,4 @@ CREATE TABLE Fact_AcademicPerformance (
     Credits_Earned INT NOT NULL,
     Is_Pass_Flag CHAR(1) NOT NULL,     
     Is_Withdraw_Flag CHAR(1) NOT NULL  
-);
-/* Staging Tables - Just buckets for raw CSV data */
-
-CREATE TABLE Staging_Departments (
-    Department_ID_Source VARCHAR(50),
-    Department_Name VARCHAR(100),
-    College_Name VARCHAR(100),
-    Dean_Name VARCHAR(100)
-);
-
-CREATE TABLE Staging_Students (
-    Student_ID_Source VARCHAR(50),
-    Major_Department_ID_Source VARCHAR(50),
-    Last_Name VARCHAR(100),
-    First_Name VARCHAR(100),
-    Birth_Date VARCHAR(50), /* Load as string first to avoid date format errors */
-    Home_State VARCHAR(50),
-    Enrollment_Date VARCHAR(50),
-    Financial_Aid_Status VARCHAR(50),
-    Academic_Standing VARCHAR(50),
-    Housing_Status VARCHAR(50)
-);
-
-CREATE TABLE Staging_Courses (
-    Course_ID_Source VARCHAR(50),
-    Department_ID_Source VARCHAR(50),
-    Course_Name VARCHAR(100),
-    Course_Credit VARCHAR(10),
-    Course_Level VARCHAR(10),
-    Is_Gateway_Course_Flag VARCHAR(10)
-);
-
-CREATE TABLE Staging_Professors (
-    Professor_ID_Source VARCHAR(50),
-    Professor_First_Name VARCHAR(100),
-    Professor_Last_Name VARCHAR(100),
-    Department_ID_Source VARCHAR(50)
-);
-
-CREATE TABLE Staging_Semesters (
-    Semester_ID_Source VARCHAR(50),
-    Semester_Year VARCHAR(50),
-    Semester_Name VARCHAR(50)
-);
-
-CREATE TABLE Staging_Enrollments (
-    Student_ID_Source VARCHAR(50),
-    Course_ID_Source VARCHAR(50),
-    Professor_ID_Source VARCHAR(50),
-    Semester_ID_Source VARCHAR(50),
-    Letter_Grade VARCHAR(10)
 );
